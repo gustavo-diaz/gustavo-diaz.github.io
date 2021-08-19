ffd053ed69aeec884dedd121fc4aca7389a20e07
gustavo-diaz <diazdia2@illinois.edu>
2021-08-19 12:13:40 -0500

    fixes

--- teaching/cp_intro/slides/makefile.R
+++ teaching/cp_intro/slides/makefile.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ###### COMPILE SLIDES ####
 # Use inline for each separate file
 # Run as a job to compile all at once
@@ -11,17 +10,3 @@ pagedown::chrome_print(here::here("teaching/cp_intro/slides/1a_intro.Rmd"))
 
 #This works as long as I don't care about interactive content nor incremental slides
 
-=======
-###### COMPILE SLIDES ####
-# Use inline for each separate file
-# Run as a job to compile all at once
-
-# Install packages if needed
-# install.packages(c("xaringan", "xaringanthemer", "pagedown", "here"))
-
-# Then run
-pagedown::chrome_print(here::here("teaching/cp_intro/slides/1a_intro.Rmd"))
-
-#This works as long as I don't care about interactive content nor incremental slides
-
->>>>>>> c79f288f9e5b2e307a22fb1e36ee37080c13bbe1
