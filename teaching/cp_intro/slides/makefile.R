c79f288f9e5b2e307a22fb1e36ee37080c13bbe1
Gustavo Díaz <gustavodiaz@tulane.edu>
2021-08-19 11:56:34 -0500

    upload teaching materials manually
    manually because arrgghhhh

---
+++ teaching/cp_intro/slides/makefile.R
@@ -0,0 +1,12 @@
+###### COMPILE SLIDES ####
+# Use inline for each separate file
+# Run as a job to compile all at once
+
+# Install packages if needed
+# install.packages(c("xaringan", "xaringanthemer", "pagedown", "here"))
+
+# Then run
+pagedown::chrome_print(here::here("teaching/cp_intro/slides/1a_intro.Rmd"))
+
+#This works as long as I don't care about interactive content nor incremental slides
+
