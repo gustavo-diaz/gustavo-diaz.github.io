f105e85742d81f68a0f3d32628466b776d58ab17
gustavo-diaz <diazdia2@illinois.edu>
2021-09-08 13:55:30 -0500

    revise cp 2_politics

--- teaching/cp_intro/slides/makefile.R
+++ teaching/cp_intro/slides/makefile.R
@@ -8,7 +8,7 @@
 # Then run
 pagedown::chrome_print(here::here("teaching/cp_intro/slides/1a_intro.Rmd"))
 pagedown::chrome_print(here::here("teaching/cp_intro/slides/1b_science.Rmd"))
-pagedown::chrome_print(here::here("teaching/cp_intro/slides/2a_politics.Rmd"))
+pagedown::chrome_print(here::here("teaching/cp_intro/slides/2_politics.Rmd"))
 
 #This works as long as I don't care about interactive content nor incremental slides
 
